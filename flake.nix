b1bc3374e0e7f7768191717d3b85fd27e629864e
vkcku <git@mail.vkcku.com>
2025-10-23 09:59:09 +0530

    chore: setup basic nix flake checks

--- flake.nix
+++ flake.nix
@@ -9,22 +9,56 @@
     { nixpkgs, ... }:
     let
       system = "x86_64-linux";
+      lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
+
+      formatters = with pkgs; [
+        # keep-sorted start
+        keep-sorted
+        nixfmt-rfc-style
+        python313Packages.mdformat
+        python313Packages.mdformat-gfm
+        taplo
+        treefmt
+        typos
+        # keep-sorted end
+      ];
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # Formatters/linters.
-          # keep-sorted start
-          keep-sorted
-          nixfmt-rfc-style
-          python313Packages.mdformat
-          python313Packages.mdformat-gfm
-          taplo
-          treefmt
-          typos
-          # keep-sorted end
-        ];
+        buildInputs = formatters;
       };
+
+      checks."${system}" =
+        let
+          /**
+            Ensure the `$out` directory is created since the derivation will be
+            marked as failed otherwise.
+          */
+          mkScript = script: script + "\n" + "mkdir $out";
+
+          mkChecks =
+            checks:
+            lib.attrsets.mapAttrs (
+              name: check:
+              pkgs.runCommandLocal name {
+                src = ./.;
+                nativeBuildInputs = check.buildInputs;
+                dontBuild = true;
+              } (mkScript check.script)
+            ) checks;
+        in
+        mkChecks {
+          fmt-lint = {
+            buildInputs = [ formatters ];
+            script = ''
+              treefmt \
+                --config-file "$src/treefmt.toml" \
+                --ci \
+                --tree-root "$src" \
+                --walk filesystem
+            '';
+          };
+        };
     };
 }
