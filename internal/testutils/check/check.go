e1f86a6c3695862ff1c7c62e50b5c4dcbe99371b
vkcku <git@mail.vkcku.com>
2025-10-23 21:46:09 +0530

    test: colored diffs

--- internal/testutils/check/check.go
+++ internal/testutils/check/check.go
@@ -4,9 +4,18 @@
 package check
 
 import (
+	"flag"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+)
+
+var useColor = flag.Bool( //nolint:gochecknoglobals
+	"pastyears.color",
+	true,
+	"If enabled, display colored diffs in test outputs.",
 )
 
 // Equal checks if the given values are equal. If they are not equal, then the
@@ -19,8 +28,36 @@ func Equal[T any](t *testing.T, want T, got T) bool {
 	equal := diff == ""
 
 	if equal == false {
-		t.Error(diff)
+		t.Error(ansiDiff(diff))
 	}
 
 	return equal
+}
+
+func ansiDiff(diff string) string {
+	// REFERENCE:
+	// https://github.com/google/go-cmp/issues/230#issuecomment-665750648
+	if diff == "" {
+		return ""
+	}
+
+	if *useColor == false {
+		return diff
+	}
+
+	lines := strings.Split(diff, "\n")
+	for i, s := range lines {
+		switch {
+		case strings.HasPrefix(s, "-"):
+			lines[i] = escapeCode(31) + s + escapeCode(0)
+		case strings.HasPrefix(s, "+"):
+			lines[i] = escapeCode(32) + s + escapeCode(0)
+		}
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func escapeCode(code int) string {
+	return fmt.Sprintf("\x1b[%dm", code)
 }
