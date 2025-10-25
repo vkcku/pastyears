3d427c1fc7baf11adf9171f7deab7540b175330d
vkcku <git@mail.vkcku.com>
2025-10-25 18:16:41 +0530

    feat(cli): postgres new command

--- cmd/pastyears/main.go
+++ cmd/pastyears/main.go
@@ -13,6 +13,8 @@ import (
 	"strings"
 
 	"github.com/urfave/cli/v3"
+
+	"github.com/vkcku/pastyears/internal/postgres"
 )
 
 func main() {
@@ -20,14 +22,92 @@ func main() {
 	defer cancel()
 
 	cmd := &cli.Command{
-		Usage:    "A CLI for managing Pastyears because shell scripts suck.",
-		Commands: []*cli.Command{lintCommand(), preCommitCommand()},
+		Usage: "A CLI for managing Pastyears because shell scripts suck.",
+		Commands: []*cli.Command{
+			lintCommand(),
+			preCommitCommand(),
+			postgresCommand(),
+		},
 	}
 
 	err := cmd.Run(ctx, os.Args)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
 		os.Exit(1)
+	}
+}
+
+type errMissingValue struct {
+	field string
+}
+
+func (e errMissingValue) Error() string {
+	return e.field + " not set"
+}
+
+func postgresCommand() *cli.Command {
+	var (
+		database string
+		pgDir    string
+		port     uint16
+	)
+
+	return &cli.Command{
+		Name:    "postgres",
+		Usage:   "Manage the dev postgres instance.",
+		Aliases: []string{"pg"},
+		Commands: []*cli.Command{
+			{
+				Name:  "new",
+				Usage: "Create a new Postgres instance.",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "pgdir",
+						Usage:       "The directory to store all the postgres related files.",
+						Sources:     cli.EnvVars("PGHOST"),
+						Destination: &pgDir,
+						Validator: func(s string) error {
+							if s == "" {
+								return errMissingValue{"pgdir"}
+							}
+
+							return nil
+						},
+					},
+					&cli.StringFlag{
+						Name:        "database",
+						Usage:       "The database to create.",
+						Value:       "pastyears",
+						Sources:     cli.EnvVars("PGDATABASE"),
+						Destination: &database,
+						Validator: func(s string) error {
+							if s == "" {
+								return errMissingValue{"database"}
+							}
+
+							return nil
+						},
+					},
+					&cli.Uint16Flag{
+						Name:        "port",
+						Usage:       "The port to start postgres on listen on.",
+						Value:       5432,
+						Sources:     cli.EnvVars("PGPORT"),
+						Destination: &port,
+						Validator: func(u uint16) error {
+							if u == 0 {
+								return errMissingValue{"port"}
+							}
+
+							return nil
+						},
+					},
+				},
+				Action: func(ctx context.Context, _ *cli.Command) error {
+					return postgres.New(ctx, pgDir, port, database)
+				},
+			},
+		},
 	}
 }
 
