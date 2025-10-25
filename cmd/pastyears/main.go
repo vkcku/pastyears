4ad9531161aecee0a5155a2380085a21411eb535
vkcku <git@mail.vkcku.com>
2025-10-25 07:20:17 +0530

    feat(cli): lint command

--- cmd/pastyears/main.go
+++ cmd/pastyears/main.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"os/signal"
 
 	"github.com/urfave/cli/v3"
@@ -17,12 +18,46 @@ func main() {
 	defer cancel()
 
 	cmd := &cli.Command{
-		Usage: "A CLI for managing Pastyears because shell scripts suck.",
+		Usage:    "A CLI for managing Pastyears because shell scripts suck.",
+		Commands: []*cli.Command{lintCommand()},
 	}
 
 	err := cmd.Run(ctx, os.Args)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
 		os.Exit(1)
+	}
+}
+
+func lintCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "lint",
+		Usage: "Run all formatters and linters while autofixing issues where possible.",
+		Commands: []*cli.Command{
+			{
+				Name:  "go",
+				Usage: "Lint all go files.",
+				Action: func(ctx context.Context, _ *cli.Command) error {
+					cmd := exec.CommandContext(
+						ctx,
+						"golangci-lint",
+						"run",
+						"--fix",
+						"./...",
+					)
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+
+					return cmd.Run()
+				},
+			},
+		},
+		Action: func(ctx context.Context, _ *cli.Command) error {
+			cmd := exec.CommandContext(ctx, "treefmt")
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			return cmd.Run()
+		},
 	}
 }
