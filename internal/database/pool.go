0795a1616a843bbc4c2e3635be9583a817b44dd4
vkcku <git@mail.vkcku.com>
2025-11-01 06:07:05 +0530

    refactor: alias database type

--- internal/database/pool.go
+++ internal/database/pool.go
@@ -13,12 +13,16 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Database is a handle to interact with the database. It is safe for concurrent
+// usage.
+type Database = sql.DB
+
 // ErrEmptyFilePath is returned when no path to a file is provided when creating
 // a new connection.
 var ErrEmptyFilePath = errors.New("database: no filepath given")
 
 // New returns a new database connection pool with sensible defaults.
-func New(file string) (*sql.DB, error) {
+func New(file string) (*Database, error) {
 	// TODO: Wrap the sqlite3 driver to add hooks so that queries can be tracked
 	// and other lifecycle hooks can be added such as when opening/closing
 	// connections. For example, this would be useful to run `PRAGMA optimize`
